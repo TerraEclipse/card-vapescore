aa8fd1d4e8fd416324b45a70b99c4ebcfdb13d9f
Nick Badagliacca <nick@terraeclipse.com>
2016-10-26 13:44:34 -0400

    Add script array to card.json

--- js/bg.js
+++ js/bg.js
@@ -1,5 +1,4 @@
-$(function () {
-  function setBackground () {
+window.onload = function setBackground () {
     var card = document.getElementsByClassName('card')[0]
     var bg = document.getElementsByClassName('background')[0]
     var state = card.getAttribute('data-state')
@@ -9,4 +8,4 @@ $(function () {
     bg.setAttribute('style', css)
   }
   setBackground()
-});
+}
